3d7c81a9269c3b716fb28019b5b597dc375ee1f4
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-15 20:48:53 +0300

    4.2.11

--- Program.cs
+++ Program.cs
@@ -4,9 +4,49 @@ namespace SF4._2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+		static void Main(string[] args)
+		{
+			int t = 0;
+			do
+			{
+				Console.WriteLine("Итерация {0}.", t);
+				Console.WriteLine("Введите цвет: ");
+				var color = Console.ReadLine();
+
+				switch (color)
+				{
+					case "red":
+						Console.BackgroundColor = ConsoleColor.Red;
+						Console.ForegroundColor = ConsoleColor.Black;
+
+						Console.WriteLine("Your color is red!");
+						break;
+
+					case "green":
+						Console.BackgroundColor = ConsoleColor.Green;
+						Console.ForegroundColor = ConsoleColor.Black;
+
+						Console.WriteLine("Your color is green!");
+						break;
+
+					case "cyan":
+						Console.BackgroundColor = ConsoleColor.Cyan;
+						Console.ForegroundColor = ConsoleColor.Black;
+
+						Console.WriteLine("Your color is cyan!");
+						break;
+
+					default:
+						Console.BackgroundColor = ConsoleColor.Yellow;
+						Console.ForegroundColor = ConsoleColor.Red;
+
+						Console.WriteLine("Your color is yellow!");
+						break;
+
+				}
+				t++;
+			} while (t <= 2);
+			Console.WriteLine("Конец цикла, счетчик = {0}.", t);
+		}
+	}
 }
